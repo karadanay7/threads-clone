c1440c21a75cb25467a2217f4ddff8d2039f228a
Aysegul <karadan.ay7@gmail.com>
2023-09-16 22:47:08 +0300

    first commit of the threads-clone project

---
+++ nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  pages: true,
+  devtools: { enabled: false },
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL,
+    },
+  },
+  modules: [
+    "nuxt-icon",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vite-pwa/nuxt",
+    "@nuxtjs/supabase",
+  ],
+  supabase: {
+    redirect: false,
+  },
+});
